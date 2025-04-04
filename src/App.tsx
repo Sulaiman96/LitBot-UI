b9359b8b9c7b97840c3e49745091b13d3a7cdf3c
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2025-04-05 00:33:34 +0100

    Initial commit with Vite, Tailwind, and Framer Motion Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen bg-black text-white">
+      <h1 className="text-center text-4xl font-bold pt-20">
+        LitBot Landing Page
+      </h1>
+    </div>
+  );
+}
+
+export default App;
